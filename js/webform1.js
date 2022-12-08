1fa4ec7967fdfe11813fabba2bc28398e8688da0
rasikkunwar <rasikkunwar31@gmail.com>
2022-12-07 22:01:47 -0600

    closure assignment uploaded

--- js/webform1.js
+++ js/webform1.js
@@ -1,12 +1,12 @@
-window.onload = function(){
-    document.getElementById('submitBtn').onclick = function(event){
+window.onload = function () {
+    document.getElementById('submitBtn').onclick = function (event) {
         event.preventDefault();
         let passwordVal = document.getElementById('password').value;
         let passwordMatch = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}$/;
-        if(passwordMatch.test(passwordVal)){
+        if (passwordMatch.test(passwordVal)) {
             alert('Form submitted successfully');
         }
-        else{
+        else {
             alert('Password field is at least 10 characters in length and contain at least one number and one uppercase and lowercase letter');
         }
     }
