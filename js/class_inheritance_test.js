5decd7add600379aae22c857914c7430dbac3cb5
rasikkunwar <rasikkunwar31@gmail.com>
2022-12-12 20:16:53 -0600

    class part II assignments

--- js/class_inheritance_test.js
+++ js/class_inheritance_test.js
@@ -1,12 +1,12 @@
 describe("SavingAccounts", function () {
     it("Add interest to the account", function () {
       let accountNumber = 1;
-      let interestRate = 15;
+      let interestRate = 20;
       let savingAccount = new SavingsAccount(accountNumber, interestRate);
       savingAccount.deposit(200);
       savingAccount.addInterest();
       let actualAmount = savingAccount.getBalance();
-      let expectedAmount = 230;
+      let expectedAmount = 240;
       assert.equal(actualAmount, expectedAmount);
     });
   });
@@ -14,17 +14,17 @@ describe("SavingAccounts", function () {
   describe("CheckingAccounts", function () {
     it("Withdraw amount from account", function () {
       let accountNumber = 1;
-      let overdraftLimit = 50;
+      let overdraftLimit = 100;
       let checkingAccount = new CheckingAccount(accountNumber, overdraftLimit);
       checkingAccount.deposit(500);
-      checkingAccount.withdraw(200);
+      checkingAccount.withdraw(300);
       let actualAmount = checkingAccount.getBalance();
-      let expectedAmount = 300;
+      let expectedAmount = 200;
       assert.equal(actualAmount, expectedAmount);
     });
     it("Check for error if zero amount is enter to withdraw", function () {
       let accountNumber = 1;
-      let overdraftLimit = 50;
+      let overdraftLimit = 1000;
       let checkingAccount = new CheckingAccount(accountNumber, overdraftLimit);
       checkingAccount.deposit(500);
       chai
@@ -38,12 +38,12 @@ describe("SavingAccounts", function () {
       let bank = new Bank();
       bank.addAccount(50);
       bank.addCheckingAccount(100, 50);
-      bank.addSavingsAccount(200, 15);
+      bank.addSavingsAccount(200, 20);
       let actualReport = bank.accountReport();
       let expectedReport =
         "Account 1: balance 50\n" +
         "CheckingAccount 2: balance 100 overdraft limit: 50\n" +
-        "SavingsAccount 3: balance 200 interest rate: 15\n";
+        "SavingsAccount 3: balance 200 interest rate: 20\n";
       assert.equal(actualReport, expectedReport);
     });
     it("Close account", function () {
@@ -51,12 +51,12 @@ describe("SavingAccounts", function () {
       let bank = new Bank();
       bank.addAccount(50);
       bank.addCheckingAccount(100, 50);
-      bank.addSavingsAccount(200, 15);
+      bank.addSavingsAccount(200, 20);
       bank.closeAccount(2);
       let actualReport = bank.accountReport();
       let expectedReport =
         "Account 1: balance 50\n" +
-        "SavingsAccount 3: balance 200 interest rate: 15\n";
+        "SavingsAccount 3: balance 200 interest rate: 20\n";
       assert.equal(actualReport, expectedReport);
     });
     it("End of Month", function () {
@@ -64,14 +64,14 @@ describe("SavingAccounts", function () {
       let bank = new Bank();
       bank.addAccount(50);
       bank.addCheckingAccount(100, 50);
-      bank.addSavingsAccount(200, 15);
+      bank.addSavingsAccount(200, 20);
       //withdrawing 120 from checking account
       bank.getAccount(2).withdraw(120);
       let actualReport = bank.endOfMonth();
       let expectedReport =
         "\n" +
         "Warning, low balance CheckingAccount 2: balance -20 overdraft limit: 50\n" +
-        "Interest added SavingsAccount3: balance:230 interest:30\n";
+        "Interest added SavingsAccount3: balance:240 interest:40\n";
       assert.equal(actualReport, expectedReport);
     });
   });
